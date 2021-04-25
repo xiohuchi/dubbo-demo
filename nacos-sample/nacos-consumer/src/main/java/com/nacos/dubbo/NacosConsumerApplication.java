9a893df5ae017bbac3b5d4a0fe2d45fe6002c911
杨滨 <bin.yang3@zhenai.com>
2021-04-25 17:21:52 +0800

    dubbo-demo

---
+++ nacos-sample/nacos-consumer/src/main/java/com/nacos/dubbo/NacosConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.nacos.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 杨滨
+ * @date 2021年04月25日
+ */
+@SpringBootApplication
+public class NacosConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConsumerApplication.class, args);
+    }
+}
---
+++ nacos-sample/nacos-provider/src/main/java/com/nacos/dubbo/NacosProviderApplication.java
@@ -0,0 +1,12 @@
+package com.nacos.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderApplication.class, args);
+    }
+}
